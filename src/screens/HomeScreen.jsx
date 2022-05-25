fd4db045307d64aa76eb784be3250025ec8f81dd
bitkunst <bitkunst23@gmail.com>
2022-05-25 15:51:10 +0900

    .

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -5,14 +5,8 @@ import { styles } from "../public/styles";
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Button
-        title="건배사"
-        onPress={() => navigation.navigate("Toast")}
-      ></Button>
-      <Button
-        title="술게임"
-        onPress={() => navigation.navigate("Game")}
-      ></Button>
+      <Button title="건배사" onPress={() => navigation.navigate("Toast")} />
+      <Button title="술게임" onPress={() => navigation.navigate("Game")} />
       <StatusBar style="auto" />
     </View>
   );
