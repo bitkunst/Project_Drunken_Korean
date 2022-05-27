ca763b023a86e97524a63024cafdfa4652e4ea11
bitkunst <bitkunst23@gmail.com>
2022-05-27 21:37:39 +0900

    feat: toast list screen

--- src/components/ToastBtn.jsx
+++ src/components/ToastBtn.jsx
@@ -1,5 +1,16 @@
-import { View, Pressable, Text } from "react-native";
+import {
+  View,
+  Pressable,
+  TouchableHighlight,
+  Text,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
 import { styles } from "../public/styles";
+const { width, height } = Dimensions.get("screen");
+
+const hardListBg = require("../../assets/img/hard_list.jpeg");
+const easyListBg = require("../../assets/img/easy_list.jpeg");
 
 const ToastBtn = ({ navigation }) => {
   return (
@@ -13,24 +24,57 @@ const ToastBtn = ({ navigation }) => {
       }}
     >
       <>
-        <Pressable
+        <TouchableHighlight
           onPress={() =>
             navigation.navigate("ToastDesc", {
               isEasy: false,
             })
           }
+          underlayColor="#B1934B"
+          activeOpacity={1}
         >
-          <Text style={styles.titleSt}>회식 자리</Text>
-        </Pressable>
-        <Pressable
+          <ImageBackground
+            source={hardListBg}
+            resizeMode="stretch"
+            style={{
+              height: height * 0.5,
+              width,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            imageStyle={{
+              opacity: 0.7,
+            }}
+          >
+            <Text style={{ ...styles.titleSt }}>회식 자리</Text>
+          </ImageBackground>
+        </TouchableHighlight>
+        <View style={{ borderStyle: "solid", borderWidth: 1, width }}></View>
+        <TouchableHighlight
           onPress={() =>
             navigation.navigate("ToastDesc", {
-              isEasy: true,
+              isEasy: false,
             })
           }
+          underlayColor="#B1934B"
+          activeOpacity={1}
         >
-          <Text style={styles.titleSt}>편한 자리</Text>
-        </Pressable>
+          <ImageBackground
+            source={easyListBg}
+            resizeMode="stretch"
+            style={{
+              height: height * 0.5,
+              width,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            imageStyle={{
+              opacity: 0.7,
+            }}
+          >
+            <Text style={{ ...styles.titleSt }}>편한 자리</Text>
+          </ImageBackground>
+        </TouchableHighlight>
       </>
     </View>
   );
